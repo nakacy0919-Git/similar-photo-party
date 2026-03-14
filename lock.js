20f678805f51783eab9786363eddd5c5e4ac58e3
NAKACY0919 <NAKACY0919@GMAIL.com>
2026-03-14 09:41:49 +0900

    パスワードロックの不具合修正

--- lock.js
+++ lock.js
@@ -1,5 +1,5 @@
 // === パスワード設定 ===
-const CORRECT_PASSWORD = "party9"; // ここに好きなパスワードを設定してください
+const CORRECT_PASSWORD = "party"; // ここに好きなパスワードを設定してください
 
 // 画面が開いた瞬間にパスワード入力を求める
 const userInput = prompt("パスワードを入力してください:");
@@ -7,6 +7,6 @@ const userInput = prompt("パスワードを入力してください:");
 // パスワードが間違っている、またはキャンセルを押した場合
 if (userInput !== CORRECT_PASSWORD) {
     alert("パスワードが違います。アクセスできません。");
-    // 画面の中身を真っ白にして、エラーメッセージだけにする
-    document.body.innerHTML = "<h1 style='text-align:center; margin-top:20vh; font-family:sans-serif;'>アクセス制限されています🔒</h1>";
+    // 【修正箇所】間違えたら強制的に真っ白なページ（about:blank）に飛ばして追い出す！
+    window.location.href = "about:blank";
 }
